0b25b9b2953e01dbb559f3f8a9696622d42ce2a8
Kevin Neaton <neatonk@gmail.com>
2013-01-10 15:14:11 -0600

    deploy snapshot deps to 'snapshots' by default and pacth bug in leiningen.deploy
    * resolving repo for each dep unless repository name is specified
    * leiningen.deploy was not detecting file uri's correctly

--- src/leiningen/deploy_deps.clj
+++ src/leiningen/deploy_deps.clj
@@ -18,6 +18,24 @@
         :else message))
 
 
+(defn snapshot-jar? [jar-file]
+  (re-find #"SNAPSHOT" (.getName jar-file)))
+
+;;TODO: is this logic already available in leiningen.core?
+(defn- default-repo-name [jar-file]
+  (if (snapshot-jar? jar-file)
+    "snapshots"
+    "releases"))
+
+(defn repo-for
+  "Returns the repository for project matching repository-name (if
+  given) or the correct default repository for the jar-file (if
+  given). Otherwise returns nil."
+  [project {:keys [jar-file repository-name]}]
+  (cond repository-name (deploy/repo-for project repository-name)
+        jar-file (deploy/repo-for project (default-repo-name jar-file))))
+
+
 ;; Alias get-dependencies because it is priavte...
 (def get-dependencies #'leiningen.core.classpath/get-dependencies)
 
@@ -32,30 +50,51 @@
 
 
 (defn files-for
-  "Returns a lazy seq of dependency file info as kvs ready for deploy."
+  "Returns a lazy seq of dependency file maps ready for deploy."
   [project]
   (let [deps (deps-for project)
         jars (map (comp :file meta) deps)
         poms (poms-for jars)]
-    (assert (= (count deps) (count jars) (count poms)))
+    (main/debug "Dependencies for: " project "\n\n" deps)
+    (assert (not-any? nil? (concat deps jars poms)))
     (map (fn [dep jar pom]
-           [:coordinates dep
+           {:coordinates dep
             :jar-file jar
-            :pom-file pom])
+            :pom-file pom})
          deps jars poms)))
+
+
+;; Copied from leiningen.deploy source to fix detection of file uri's
+;; and avoid returning nil settings.
+;; TODO: create a pull request for this!
+(defn add-auth-interactively [[id settings]]
+  (if (or (and (:username settings) (some settings [:password :passphrase :private-key-file]))
+          (.startsWith (:url settings) "file:/"))
+    [id settings]
+    (do
+      (println "No credentials found for" id)
+      (println "See `lein help deploying` for how to configure credentials.")
+      (print "Username: ") (flush)
+      (let [username (read-line)
+            password (.readPassword (System/console) "%s"
+                                    (into-array ["Password: "]))]
+        [id (assoc settings :username username :password password)]))))
+
 
 (defn deploy-deps
   "Deploy project dependencies to a remote repository."
   [project & [repository-name]]
-  (let [repo (deploy/repo-for project repository-name)]
-    (main/debug "Deploying deps for" (pr-str project) "to" (pr-str repo))
-    (try
-      (doseq [files (files-for project)]
-        (main/debug "Deploying" files "to" repo)
-        (apply aether/deploy
-               :transfer-listener :stdout
-               :repository [repo]
-               files))
-      (catch org.sonatype.aether.deployment.DeploymentException e
-        (when main/*debug* (.printStackTrace e))
-        (main/abort (abort-message (.getMessage e)))))))
+  (with-redefs [deploy/add-auth-interactively add-auth-interactively]
+    (let [repo (repo-for project {:repository-name repository-name})]
+      (try
+        (doseq [files (files-for project)]
+          (let [repo (or repo (repo-for project files))]
+            (main/debug "Deploying" files "to" repo)
+            (apply aether/deploy
+                   (apply concat
+                          [:transfer-listener :stdout
+                           :repository [repo]]
+                          files))))
+        (catch org.sonatype.aether.deployment.DeploymentException e
+          (when main/*debug* (.printStackTrace e))
+          (main/abort (abort-message (.getMessage e))))))))
